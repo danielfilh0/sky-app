a43ed8a52c2befc169aad6433a3eb0b7c9b7cfef
Daniel Filho <danbsilva98@gmail.com>
2021-03-04 10:35:58 -0300

    without api

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ function main() {
 }
 
 function searchLocation(location) {
-  const apiKey = '0d571b1017b8214d63acaf9778ec017b';
+  const apiKey = ''; // inserir API
   const weatherFetch = fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${location}&lang=pt_br&units=metric&appid=${apiKey}`,
   );
